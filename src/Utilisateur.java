63a3e8be60488de263bb15993b6faed4f39aeec2
yannis.mennaï <yannis.mennai@gmail.com>
2020-07-02 12:29:47 +0200

    modif classe Utilisateur

--- src/Utilisateur.java
+++ src/Utilisateur.java
@@ -22,7 +22,15 @@ public class Utilisateur {
 		this.nom = nom;
 		this.prenom = prenom;
 	}
-		
+	
+	
+	
+	public Utilisateur(String nom, String prenom) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+
 	/*____________ G / S _____________*/
 	
 
