c7b9df0c0553727cfde4d6235133535a733b6b86
yannis.mennaï <yannis.mennai@gmail.com>
2020-07-02 11:49:52 +0200

    ajout des getters/setters à la classe Utilisateur

--- src/Utilisateur.java
+++ src/Utilisateur.java
@@ -22,7 +22,33 @@ public class Utilisateur {
 		this.nom = nom;
 		this.prenom = prenom;
 	}
+		
+	/*____________ G / S _____________*/
 	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
 	
 	
 }//end class
